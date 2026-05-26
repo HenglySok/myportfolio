47cbde0052f33635493e35d059c60b5395846f3d
henglysok <sokhenglystudent@gmail.com>
2026-05-27 01:50:32 +0700

    Fix image paths for Vite/public and update .gitignore

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,21 +1,21 @@
 const projects = [
     {
         name: "Manga Reader App",
-        img: "/public/project/manga.png",
+        img: "/project/manga.png",
         technology: ["React", "Tailwind CSS"],
         repository: "https://github.com/HenglySok/demomaga",
         demo: "https://henglysok.github.io/demomaga/",
     },
     {
         name: "Khmer Traditional Games",
-        img: "/public/project/khmer_traditional_game.png",
+        img: "/project/khmer_traditional_game.png",
         technology: ["Flutter", "Dart"],
         repository: "https://github.com/HenglySok/tradiinal_game",
         demo: "",
     },
     {
         name: "Khmer Traditional Games UX/UI Design",
-        img: "/public/project/figma.png",
+        img: "/project/figma.png",
         technology: ["Figma"],
         repository: "",
         demo: "https://www.figma.com/design/BjB5HKAFLtv2HtkUsm7EkS/Flutter?node-id=408-875&t=gcv6RsrYJiCtxY79-1",
